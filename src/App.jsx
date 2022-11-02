f83309719e453b740238b58e2cbb9e89bc7c7a0e
Min Jun Kim <d0uwhs@gmail.com>
2022-11-02 19:57:31 +0900

    Fix: Remove Dangling Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router";
 
 import NotFoundPage from "@pages/NotFoundPage";
 import MenuPage from "@pages/MenuPage";
-import RoleGroupPage from "@pages/role/RoleGroupPage";
 import EmpInformationPage from "@pages/emp/EmpInformationPage";
 import IndexPage from "@pages/IndexPage";
 import ComBasicInfoPage from "@pages/company/ComBasicInfoPage";
@@ -17,9 +16,6 @@ const App = () => (
     <Route path="/" component={IndexPage} exact />
     <Route path="/menu" component={MenuPage} />
     <Route path="/userrole" component={UserRolePage} />
-    <Route path="/role">
-      <Route path="/role/group" component={RoleGroupPage} />
-    </Route>
     <Route path="/employee">
       <Route path="/employee/info" component={EmpInformationPage} />
     </Route>
