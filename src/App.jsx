4757e5f6a704a5d72dcd69b1222a9a29279a3a50
jeongsuyeon <tndus3822@gmail.com>
2022-11-02 10:04:47 +0900

    employee info(사원 관리) 페이지 디자인 작업중

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import IndexPage from "@pages/IndexPage";
 import NotFoundPage from "@pages/NotFoundPage";
+import EmployeeInfo from "@pages/Employee/EmployeeInfo";
+import IndexPage from "@pages/IndexPage";
 
 const App = () => (
   <Switch>
     <Route path="/" component={IndexPage} exact />
+    <Route path="/employee">
+      <Route path="/employee/info" component={EmployeeInfo} />
+    </Route>
     <Route component={NotFoundPage} />
   </Switch>
 );
---
+++ src/pages/Employee/EmployeeInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function EmployeeInfo(props) {
+  return (
+    <>
+      <div>
+        <span>사용자 : n 명</span> <span>정렬▼</span>
+      </div>
+      <div>
+        <div>사진</div>
+      </div>
+    </>
+  );
+}
+
+export default EmployeeInfo;
