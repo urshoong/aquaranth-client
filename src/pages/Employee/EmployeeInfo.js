1d0b45d4d2fd9ee5aff55aeebd0e36c8f93ee376
jeongsuyeon <tndus3822@gmail.com>
2022-11-02 14:57:55 +0900

    employee info 디자인 진행중 (pull request test)

--- src/pages/Employee/EmployeeInfo.js
+++ src/pages/Employee/EmployeeInfo.js
@@ -1,15 +1,35 @@
 import React from "react";
+import "./EmployeeStyle.css";
+
+// TODO 정렬, 페이징
 
 function EmployeeInfo(props) {
   return (
-    <>
-      <div>
+    <div className="pageLine">
+
+      <div className="empList"> div2-1
         <span>사용자 : n 명</span> <span>정렬▼</span>
+        <div className="empItem">
+          <div className="empItemImg">사진 </div>
+          <div className="empIdName">
+            <div>
+              ID
+            </div>
+            <div>
+              NAME
+            </div>
+          </div>
+          <div>
+            dd
+          </div>
+        </div>
       </div>
-      <div>
-        <div>사진</div>
+
+      <div className="infoPage">2-2
+        <span>selectOne</span>
       </div>
-    </>
+
+    </div>
   );
 }
 
