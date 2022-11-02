2630c5f5683dc828e2ff5ee7540857b43efad6b3
d0uwhs <d0uwhs@gmail.com>
2022-11-02 15:49:48 +0900

    Feat: 메뉴 조회

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import IndexPage from "@pages/IndexPage";
 import NotFoundPage from "@pages/NotFoundPage";
+import MenuPage from "@pages/MenuPage";
 
 const App = () => (
   <Switch>
     <Route path="/" component={IndexPage} exact />
+    <Route path="/menu" component={MenuPage}/>
     <Route component={NotFoundPage} />
   </Switch>
 );
