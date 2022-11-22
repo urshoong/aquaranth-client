edbf1b72d46ec7f9ec2d2012241a5c6d60a67728
Min Jun Kim <d0uwhs@gmail.com>
2022-11-22 18:18:54 +0900

    Fix: API 수정

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalSlice from "@reducer/modalSlice";
+import roleGroupSlice from "@reducer/roleGroupSlice";
 
 export const store = configureStore({
   reducer: {
     modal: modalSlice,
+    roleGroup: roleGroupSlice,
   },
 });
