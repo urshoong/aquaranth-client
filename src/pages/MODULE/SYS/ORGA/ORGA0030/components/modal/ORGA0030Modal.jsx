0b84771204e4e6b93ed69132879cc45086d93579
jeongsuyeon <tndus3822@gmail.com>
2022-11-25 16:27:35 +0900

    Employee 사원의 회사 변경 팝업 생성

--- src/pages/MODULE/SYS/ORGA/ORGA0030/components/modal/ORGA0030Modal.jsx
+++ src/pages/MODULE/SYS/ORGA/ORGA0030/components/modal/ORGA0030Modal.jsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "@components/modal/Modal";
 import useModal from "@hooks/useModal";
 import { CenterGrid, Divider, Span } from "@components/Grid";
+import { useHistory } from "react-router-dom";
+import request from "../../../../../../../utils/axiosUtil";
+
+const empInfo = async () => {
+  const { data } = await request.get("/emp/loginlist");
+  console.log(data);
+  return data;
+};
+
+const registerLoginUser = async (user) => {
+  const { data } = await request.post("/emp/registerLoginUser", user);
+  return data;
+};
+
+const initState = {
+  loginCompanyNo: 0,
+  loginDeptNo: 0,
+};
 
 const ORGA0030Modal = (props) => {
+  const [employeeState, setEmployeeState] = useState([]);
+  const [selectLogin, setSelectLogin] = useState(initState);
+  const history = useHistory();
+
+  // 회사, 부서 set
+  useEffect(() => {
+    empInfo().then((data) => {
+      console.log(data);
+      setEmployeeState(data);
+    });
+  }, []);
+
+  // 라디오 버튼 변경
+  const handleOnChangeRadio = (e) => {
+    const { value } = e.target;
+    setSelectLogin({ ...selectLogin, loginCompanyNo: value });
+  };
+
+  // select 변경
+  const handleOnChangeSelect = (e) => {
+    const { value } = e.target;
+    setSelectLogin({ ...selectLogin, loginDeptNo: value });
+  };
+
+  // 회사 변경 확인 클릭 버튼
+  const handleOnClickChangeDeptSubmit = (e) => {
+    console.log(selectLogin);
+    registerLoginUser(selectLogin).then(() => {
+      // TODO 메인페이지로 histroy, 체크박스 눌리면 select값도 같이 못가져오나..
+    });
+  };
+
+  // 모달 close
   const { closeModal } = useModal();
 
   const handleCloseModal = () => {
@@ -13,15 +64,47 @@ const ORGA0030Modal = (props) => {
   return (
     <Modal
       onClose={handleCloseModal}
-      title="ORGA0030"
+      title="접속 회사 선택"
     >
       <CenterGrid>
-        <Divider span="12">
-          {props.menucode}
-        </Divider>
-        <Divider span="12">
-          {props.menuname}
-        </Divider>
+        <div>
+          {employeeState.map((info) => {
+            return (
+              <div key={info.empNo}>
+                <input value={info.empName} name="empName" readOnly />
+                {info.lastLoginIp}
+                {info.lastLoginTime}
+                {info.companyList.map((company) => {
+                  return (
+                    <div key={company.companyNo}>
+                      <input
+                        name="loginCompanyNo"
+                        type="radio"
+                        value={company.companyNo}
+                        onChange={(e) => { handleOnChangeRadio(e); }}
+                        readOnly
+                      />
+                      {company.companyName}
+                      <select name="dept" onChange={(e) => { handleOnChangeSelect(e); }}>
+                        <option value="0">--부서 선택--</option>
+                        {company.deptList.map((dept) => {
+                          return (
+                            <option key={dept.deptNo} value={dept.deptNo} name="loginDeptNo">
+                              {dept.deptName}
+                            </option>
+                          );
+                        })}
+                      </select>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+          <button type="submit" onClick={() => { handleOnClickChangeDeptSubmit(); }}>확인</button>
+          {/* <button type="reset" onClick={() => { handleCloseModal(); }}>취소</button> */}
+        </div>
+
       </CenterGrid>
     </Modal>
   );
