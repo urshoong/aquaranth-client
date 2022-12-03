3aee80e61175c66a4b0cbda4a35f20f736ce80b3
Min Jun Kim <d0uwhs@gmail.com>
2022-12-04 05:04:39 +0900

    Fix: 시스템 모듈 메인페이지 이동 수정

--- src/pages/MODULE/SYS/index.jsx
+++ src/pages/MODULE/SYS/index.jsx
@@ -4,7 +4,6 @@ import { useHistory } from "react-router-dom";
 const Index = () => {
   const history = useHistory();
   useEffect(() => {
-    history.push("/SYS/ROLE/ROLE0030");
   }, []);
   return (
     <div />
